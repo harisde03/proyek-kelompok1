97e07f789935f473d6215786e009198b1dc5cb7c
YunitaYulianda <81976317+YunitaYulianda@users.noreply.github.com>
2021-05-23 23:05:06 +0700

    Add files via upload

--- Main.c
+++ Main.c
@@ -8,8 +8,30 @@ void enterer();         // Fungsi "tekan enter untuk lanjut"
 void drawBox();         // Gambar kotak pertanyaan dan hadiah
 void welcomeScreen();   // Selamat Datang ke WWTBM
 
+typedef struct h {
+    char *nominal;
+} Hadiah;
+
+Hadiah hadiah[15];
+
 int main() {
-    welcomeScreen();
+    hadiah[0].nominal  = "50.000";
+    hadiah[1].nominal  = "125.000";
+    hadiah[2].nominal  = "250.000";
+    hadiah[3].nominal  = "500.000";
+    hadiah[4].nominal  = "1.000.000";
+    hadiah[5].nominal  = "2.000.000";
+    hadiah[6].nominal  = "4.000.000";
+    hadiah[7].nominal  = "8.000.000";
+    hadiah[8].nominal  = "16.000.000";
+    hadiah[9].nominal  = "32.000.000";
+    hadiah[10].nominal = "64.000.000";
+    hadiah[11].nominal = "125.000.000";
+    hadiah[12].nominal = "250.000.000";
+    hadiah[13].nominal = "500.000.000";
+    hadiah[14].nominal = "1.000.000.000";
+
+    drawBox();
 
     gotoxy(0,24);
     return EXIT_SUCCESS;
@@ -22,6 +44,7 @@ void enterer() {
 }
 
 void drawBox() {
+	int i;
     system(CLEAR);
     gotoxy(0,0);
     printf("+----------------------------------------------------------------+-----------------------+\n");
@@ -31,8 +54,8 @@ void drawBox() {
     printf("+-----+----------------------------------------------------------+-----------------------+\n");
     printf("|     |\t\t\t\t\t\t\t\t |                  +--+ |\n");
 
-    for (int i = 14; i >= 0; i--) {
-        printf("|     |\t\t\t\t\t\t\t\t |                  |  | |\n");
+    for (i = 14; i >= 0; i--) {
+        printf("|     |\t\t\t\t\t\t\t\t |    %13s |  | |\n", hadiah[i].nominal);
     }
 
     printf("|     |\t\t\t\t\t\t\t\t |                  +--+ |\n");
