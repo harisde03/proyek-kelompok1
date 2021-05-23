bae79bd8613b6a3e7593d6f62f46dcf2b191f960
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2021-05-23 11:35:51 +0700

    Add files via upload

--- Main.c
+++ Main.c
@@ -8,11 +8,56 @@ void enterer();         // Fungsi "tekan enter untuk lanjut"
 void drawBox();         // Gambar kotak pertanyaan dan hadiah
 void welcomeScreen();   // Selamat Datang ke WWTBM
 
-int main() {
-    welcomeScreen();
+int main(int banyakArgumen, char *argumen[]) {
+    /*
+    FILE *fptr = fopen("database/login.bin", "wb");
+    char login[20] = "Kelompok@Satu";
 
-    gotoxy(0,24);
-    return EXIT_SUCCESS;
+    fwrite(login, sizeof(char), sizeof(login)/sizeof(char), fptr);
+    */
+
+   	if(banyakArgumen != 3) {
+       	printf("Gagal Login! Argumen tidak sesuai. Ikuti format berikut.\n");
+       	printf("./FileAplikasiProgramUtama username password\n");
+       	return 1;
+   	}
+
+   	char usernameInput[10], passwordInput[10];
+   	strcpy(usernameInput, argumen[1]);
+   	strcpy(passwordInput, argumen[2]);
+
+   	FILE *fptr;
+
+    if ((fptr = fopen("database/login.bin", "rb")) == NULL) {
+        printf("Gagal membuka file!\n");
+        return 1;
+    }
+
+    char akun[20];
+    fread(akun, sizeof(char), sizeof(akun)/sizeof(char), fptr);
+
+    fclose(fptr);
+
+    char *string[3];
+    char username[20], password[20];
+    int ctrl = 0;
+
+    string[0] = strtok(akun, "@");
+    while(string[ctrl++] != NULL) {
+        string[ctrl] = strtok(NULL, "@");
+    }
+
+    strcpy(username, string[0]);
+    strcpy(password, string[1]);
+
+    if ((strcmp(usernameInput, username) == 0) && (strcmp(passwordInput, password) == 0)) {
+        printf("Login Sebagai: %s\n", username);
+        printf("Login Berhasil\n");
+    } else {
+        printf("Login anda gagal!\n");
+    }
+
+    return 0;
 }
 
 void enterer() {
@@ -26,7 +71,7 @@ void drawBox() {
     gotoxy(0,0);
     printf("+----------------------------------------------------------------+-----------------------+\n");
     printf("|                                                                |+----+  +-----+  +----+|\n");
-    printf("|    * W H O  W A N T S  T O  B E  A  M I L L I O N A I R E *    ||    |  |     |  |    ||\n");
+    printf("|    * W H O  W A N T S  T O  B E  A  M I L L I O N A I R E *    ||CALL|  |50:50|  |POLL||\n");
     printf("|                                                                |+----+  +-----+  +----+|\n");
     printf("+-----+----------------------------------------------------------+-----------------------+\n");
     printf("|     |\t\t\t\t\t\t\t\t |                  +--+ |\n");
